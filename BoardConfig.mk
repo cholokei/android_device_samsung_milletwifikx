d570981dd23aede93049921bc914ccbb0b95abfa
cholokei <leesl0416@naver.com>
2015-12-17 01:50:32 +0900

    WIP rootdir init sepolicy ril BoardConfig.mk device.mk
    Signed-off-by: cholokei <leesl0416@naver.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,21 +22,75 @@
 # definition file).
 #
 
-# Inherit from msm8226-common
--include device/samsung/msm8226-common/BoardConfigCommon.mk
+# Inherit from qcom-common
+-include device/samsung/qcom-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/milletwifikx/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/milletwifikx/include
+
+# Architecture
+TARGET_CPU_VARIANT := krait
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := milletwifikx,SM-T330
 
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/milletwifikx/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+QCOM_BT_USE_SMD_TTY := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8226
+
+# Camera
+TARGET_PROVIDES_CAMERA_HAL := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Charger
+BOARD_BATTERY_DEVICE_NAME := "battery"
+BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
+BOARD_CHARGING_CMDLINE_VALUE := "charger"
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# CM Hardware
+###BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += \
+    hardware/cyanogen/cmhw \
+    device/samsung/milletwifikx/cmhw
+
+# Custom RIL class
+###BOARD_RIL_CLASS := ../../../device/samsung/msm8226-common/ril/
+
+# Display
+###BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
 
 # Init
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/milletwifikx/init/init_millet.c
 TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
@@ -51,14 +105,49 @@ TARGET_KERNEL_CONFIG := milletwifikx_defconfig
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Partitions
+# Partitions and Vold
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2411724800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12843662336
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8226
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+
+# Power
+TARGET_POWERHAL_SET_INTERACTIVE_EXT := device/samsung/milletwifikx/power/power_ext.c
+TARGET_POWERHAL_VARIANT := qcom
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/milletwifikx/rootdir/fstab.qcom
+
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+TARGET_PROVIDES_WCNSS_QMI        := true
+TARGET_USES_QCOM_WCNSS_QMI       := true
+TARGET_USES_WCNSS_CTRL           := true
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+
+WLAN_MODULES:
+	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
 
